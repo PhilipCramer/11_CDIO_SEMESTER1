1bd896b6886bff124f6e20a5d741fd44921892ba
fuckbrugernavne <113907244+fuckbrugernavne@users.noreply.github.com>
2022-09-26 16:38:46 +0200

    Rettelser til pointsystem

--- G11_del1/src/main/java/PointSystem.java
+++ G11_del1/src/main/java/PointSystem.java
@@ -3,8 +3,10 @@ package G11_del1.src.main.java;
 public class PointSystem {
     public PointSystem (String P1Name, String P2Name){
 
-        Player P1 = P1Name;
-        Player P2 = P2Name;
+        Player P1 = new Player();
+        Player P2 = new Player();
+        P1.setPlayerName(P1Name);
+        P2.setPlayerName(P2Name);
         P1.setPlayerScore(0);
         P2.setPlayerScore(0);
         }
