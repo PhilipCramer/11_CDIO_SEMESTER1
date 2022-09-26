38fb732cd6f52b4616591f4f1e56aaf16fbc1300
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2022-09-26 16:27:50 +0200

    Player score og Name

--- G11_del1/src/main/java/Player.java
+++ G11_del1/src/main/java/Player.java
@@ -1,2 +1,21 @@
 public class Player {
+    private String name;
+    private int score;
+    public Player(){
+        String name;
+        Integer score;
+
+    }
+    public void setPlayerName(String String){
+        name = String;
+    }
+    public void setPlayerScore(Integer Int){
+        score = Int;
+    }
+    public String getPlayerName(){
+        return name;
+    }
+    public Integer getPlayerScore(){
+        return score;
+    }
 }
