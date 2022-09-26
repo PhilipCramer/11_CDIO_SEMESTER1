0e3e84968cb130334541ce65154f99a99b50d32a
fuckbrugernavne <113907244+fuckbrugernavne@users.noreply.github.com>
2022-09-26 16:26:49 +0200

    Første layout til oprettelse af spillere

---
+++ G11_del1/src/main/java/PointSystem.java
@@ -0,0 +1,12 @@
+package G11_del1.src.main.java;
+
+public class PointSystem {
+    public PointSystem (String P1Name, String P2Name){
+
+        Player P1 = P1Name;
+        Player P2 = P2Name;
+        P1.setPlayerScore(0);
+        P2.setPlayerScore(0);
+        }
+    }
+
