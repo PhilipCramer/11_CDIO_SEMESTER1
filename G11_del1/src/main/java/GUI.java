c8b9031598e357ea0fd29e5dc5f1aef197ac724c
Philip Astrup Cramer <s224319@dtu.dk>
2022-09-26 14:29:44 +0200

    GUI version 1.1

--- G11_del1/src/main/java/GUI.java
+++ G11_del1/src/main/java/GUI.java
@@ -16,6 +16,10 @@ public class GUI {
     private JLabel playerTurn;
     private JLabel winner;
     private JLabel playerThrow;
+    private JTextField enterPlayer1Name;
+    private JTextField enterPlayer2Name;
+    private JLabel enterNames;
+    private JButton startGame;
 
     public GUI(Player p1, Player p2) {
         JFrame window = new JFrame("G11 Terning Spil");
@@ -25,19 +29,49 @@ public class GUI {
         window.setSize(500,500);
         winner.setVisible(false);
         window.setVisible(true);
-        playerTurn.setText("Player 1 har turen");
+        p1Points.setVisible(false);
+        p2Points.setVisible(false);
+        rollButton.setVisible(false);
+        playerTurn.setVisible(false);
+        playerThrow.setVisible(false);
+        terning1.setVisible(false);
+        terning2.setVisible(false);
 
 
         restartButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                Player1Name.setText("Player 1:");
+                Player2Name.setText("Player 2:");
+                enterPlayer1Name.setVisible(true);
+                enterPlayer2Name.setVisible(true);
+                p1Points.setVisible(false);
+                p2Points.setVisible(false);
+                startGame.setVisible(true);
             }
         });
 
         rollButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        startGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Player1Name.setText(enterPlayer1Name.getText() + ":");
+                Player2Name.setText(enterPlayer2Name.getText() + ":");
+                enterPlayer1Name.setVisible(false);
+                enterPlayer2Name.setVisible(false);
+                p1Points.setVisible(true);
+                p2Points.setVisible(true);
+                startGame.setVisible(false);
+                rollButton.setVisible(true);
+                playerTurn.setVisible(true);
+                playerThrow.setVisible(true);
+                terning1.setVisible(true);
+                terning2.setVisible(true);
 
             }
         });
