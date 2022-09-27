aff0bc6d8edb231bf8e11eaecffd0beab622838b
fuckbrugernavne <113907244+fuckbrugernavne@users.noreply.github.com>
2022-09-27 14:53:15 +0200

    Pointsystems koden skrevet

--- G11_del1/src/main/java/PointSystem.java
+++ G11_del1/src/main/java/PointSystem.java
@@ -1,4 +1,7 @@
 public class PointSystem {
+    private Player P1;
+    private Player P2;
+    private int WinnerPoint = 60;
     public PointSystem (String P1Name, String P2Name){
 
         Player P1 = new Player();
@@ -8,5 +11,24 @@ public class PointSystem {
         P1.setPlayerScore(0);
         P2.setPlayerScore(0);
         }
+    public String newScore(Player player, int Dice1, int Dice2){
+        if(player.getPlayerName().equals(P1.getPlayerName())){
+            P1.setPlayerScore(P1.getPlayerScore() + Dice1 + Dice2);
+            winnerCheck(player);
+        }
+        else if(player.getPlayerName().equals(P2.getPlayerName())){
+            P2.setPlayerScore(P2.getPlayerScore()+ Dice1 + Dice2);
+            winnerCheck(player);
+        }
+        else {
+           return "Ugyldigt spillernavn" ;
+        }
+        return "Ny score for " + player.getPlayerName() + " opdateret til " + player.getPlayerScore();
     }
+    public String winnerCheck(Player player){
+        if (player.getPlayerScore() >= WinnerPoint){
+        return player.getPlayerName() + " har vundet med " + player.getPlayerScore() + " point";
+       }
+    }
+}
 
