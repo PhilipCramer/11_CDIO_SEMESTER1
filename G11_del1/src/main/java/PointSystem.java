62641757b38c78dc31b8b2456f260675b25d0b24
fuckbrugernavne <113907244+fuckbrugernavne@users.noreply.github.com>
2022-09-28 12:47:46 +0200

    winnerCheck method opdateret

--- G11_del1/src/main/java/PointSystem.java
+++ G11_del1/src/main/java/PointSystem.java
@@ -1,26 +1,20 @@
 public class PointSystem {
     private int WinnerPoint = 40;
-    public PointSystem (Player P1, Player P2){
 
-        }
-    public String newScore(Player player, int Dice1, int Dice2){
-        if(player.getPlayerName().equals(P1.getPlayerName())){
-            P1.setPlayerScore(P1.getPlayerScore() + Dice1 + Dice2);
-            winnerCheck(player);
-        }
-        else if(player.getPlayerName().equals(P2.getPlayerName())){
-            P2.setPlayerScore(P2.getPlayerScore()+ Dice1 + Dice2);
-            winnerCheck(player);
-        }
-        else {
-           return "Ugyldigt spillernavn" ;
-        }
-        return "Ny score for " + player.getPlayerName() + " opdateret til " + player.getPlayerScore();
+    public PointSystem (Player P1, Player P2){
     }
-    public boolean winnerCheck(Player player){
-        if (player.getPlayerScore() >= WinnerPoint){
-        return true;
-       }
+
+    public Boolean setScore(Player player, int Dice1, int Dice2){
+        int diceSum = Dice1 + Dice2;
+        int newPlayerScore = player.getPlayerScore() + diceSum;
+        player.setPlayerScore(newPlayerScore);
+
+        return winnerCheck(player.getPlayerScore());
+    }
+    public boolean winnerCheck(int points){
+        if (points >= WinnerPoint){
+            return true;
+        }
         return false;
     }
 }
