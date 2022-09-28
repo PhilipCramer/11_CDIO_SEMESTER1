a368ef641ca38346a3944ca56cbde805ac3dea14
Philip Astrup Cramer <s224319@dtu.dk>
2022-09-28 13:26:40 +0200

    Gamecontroller v1 (Spillet kan køre i IntelliJ konsollen)

--- G11_del1/src/main/java/GameController.java
+++ G11_del1/src/main/java/GameController.java
@@ -1,41 +1,55 @@
-import javax.lang.model.element.Name;
 import java.util.Scanner;
 
 public class GameController {
     public static void main(String[] args) {
+        //instantierer Objekter og sætter spillernavne udfra bruger input
         Scanner userInput = new Scanner(System.in);
-
+        Player player1 = new Player();
+        Player player2 = new Player();
         System.out.print("Indtast navn på spiller 1: ");
-        String name1 = userInput.nextLine();
+        player1.setPlayerName(userInput.nextLine().trim());
 
         System.out.print("Indtast navn på spiller 2: ");
-        String name2 = userInput.nextLine();
+        player2.setPlayerName(userInput.nextLine().trim());
 
-        PointSystem pointSystem = new PointSystem(name1, name2);
-        //DieCup dieCup = new DieCup();
+        PointSystem pointSystem = new PointSystem(player1, player2);
+        DieCup dieCup = new DieCup(2);
         int turnCount = 1;
 
+        //Spillet kører indtil der er fundet en vinder
         do {
             System.out.println( "Stilling er:");
+            System.out.println(player1.getPlayerName() + ": " + player1.getPlayerScore());
+            System.out.println(player2.getPlayerName() + ": " + player2.getPlayerScore());
 
+            int[] playerThrow;
 
-            if(turnCount % 2 == 0){
-                System.out.println(name2 + " har turen. tryk enter for at kaste terningerne:");
+            //Vurderer ud fra turnCount hvem der har turen
+            if(turnCount % 2 == 0){ //Spiller 2 har turen
+                System.out.println(player2.getPlayerName() + " har turen. tryk enter for at kaste terningerne:");
                 userInput.nextLine();
+                playerThrow = dieCup.throwDice(2);
+                pointSystem.setScore(player2, playerThrow[0], playerThrow[1] );
 
-            }else {
-                System.out.println(name1 + " har turen. tryk enter for at kaste terningerne:");
+            }else { //Spiller 1 har turen
+                System.out.println(player1.getPlayerName() + " har turen. tryk enter for at kaste terningerne:");
                 userInput.nextLine();
+                playerThrow = dieCup.throwDice(2);
+                pointSystem.setScore(player1, playerThrow[0], playerThrow[1]);
             }
+            //Renser konsollen.
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
 
-
-
-
+            //Opdaterer turnCount
             turnCount++;
-        } while (false); //!pointSystem.winnerCheck() || !pointSystem.winnerCheck());
+        } while (!(pointSystem.winnerCheck(player1.getPlayerScore()) || pointSystem.winnerCheck(player2.getPlayerScore())));
 
-        System.out.println("Vinder");
-
-
+        if (pointSystem.winnerCheck(player1.getPlayerScore())){
+            System.out.println(player1.getPlayerName() + " har vundet med " + player1.getPlayerScore() + " point.");
+        }
+        else if (pointSystem.winnerCheck(player2.getPlayerScore())){
+            System.out.println(player2.getPlayerName() + " har vundet med " + player2.getPlayerScore() + " point.");
+        }
     }
 }
