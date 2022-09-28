20400f10609e95f1547b5e5918c9a503c25e2124
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2022-09-28 13:06:26 +0200

    Added comments to DieCup

--- G11_del1/src/main/java/DieCup.java
+++ G11_del1/src/main/java/DieCup.java
@@ -1,15 +1,21 @@
 public class DieCup {
     private static int[] DiceArray;
+    //En contructer, der laver et array af tal.
     DieCup(int DieCount) {
         DiceArray = new int[DieCount];
     }
+    //En metode der "kaster" terningerne.
     public static int[] ThrowDice(int DieCount) {
+        //Tilgiver "Cup" et array af en given længde fra contructeren.
         DieCup Cup = new DieCup(DieCount);
+        //Laver en variable (generateDie) der er en ny Dice klasse.
         Dice generateDie = new Dice();
+        //Et for loop der "kaster" terningerne og sætter hver verdi ind i DiceArray.
         for (int i = 0; i < DieCount; i++) {
             Cup.DiceArray[i] = generateDie.Dice();
         }
 
+        //Retunere et array med alle terninge verdierne.
         return Cup.DiceArray;
     }
 }
